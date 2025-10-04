89592ad6f2a7715c862bc12182eccddda0b54c41
Matthias Vallentin <matthias@vallentin.net>
2025-10-04 11:46:14 +0200

    Simplify context manager example

--- example-project/tests/python/executor-with-node-fixture/context-manager.py
+++ example-project/tests/python/executor-with-node-fixture/context-manager.py
@@ -4,8 +4,7 @@
 
 # Context-manager style: `with` calls start() on enter and stop() on exit.
 with acquire_fixture("node") as node:
-    env = node.env
     # Get the Tenzir executor, with env adapted to work with the node fixture.
-    executor = Executor.from_env(env)
+    executor = Executor.from_env(node.env)
     result = executor.run("remote { version } | summarize num_events=count()")
     print(result.stdout.decode(), end="")
